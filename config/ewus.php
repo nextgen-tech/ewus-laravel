2dc8b779f917286a4d806ef722a4ce000dbe3bd6
Krzysztof Grabania <krzysztof.grabania@etermed.pl>
2020-10-13 09:10:28 +0200

    Update configuration file

--- config/ewus.php
+++ config/ewus.php
@@ -2,22 +2,45 @@
 declare(strict_types=1);
 
 return [
+    /**
+     * Enables or disabled sandbox mode.
+     */
     'sandbox_mode' => env('EWUS_SANDBOX_MODE', false),
 
+    /**
+     * Connection used to communicate with API.
+     */
     'connection' => env('EWUS_CONNECTION', 'http'),
 
+    /**
+     * Connections configuration.
+     */
     'connections' => [
-        'http' => [],
+        'http' => [
+            'timeout' => env('EWUS_CONNECTION_TIMEOUT'),
+        ],
 
         'soap' => [],
     ],
 
+    /**
+     * Password configuration.
+     */
     'password' => [
+        /**
+         * The name of file in which will be stored password.
+         */
         'filename' => '.ewus-password',
 
+        /**
+         * The length of randomly generated password.
+         */
         'length' => env('EWUS_PASSWORD_LENGTH', 8),
     ],
 
+    /**
+     * Credentials list.
+     */
     'credentials' => [
         'domain' => env('EWUS_CREDENTIALS_DOMAIN'),
 
